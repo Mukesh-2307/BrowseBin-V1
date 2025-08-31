aa7df4f469cb5f04f4c8bee40fbcdba39264f8f8
Mukesh Kumar Dalai <mukeshdalai.2307@gmail.com>
2025-08-31 23:10:18 +0530

    update: resolved the delete button index issue.

--- controllers/deleteBookmark.js
+++ controllers/deleteBookmark.js
@@ -1,7 +1,15 @@
 import { allBookmarks } from "../data/bookmark.js";
 import renderBookmark from "./renderBookmark.js";
 
-export default function deleteBookmark(index) {
+export default function deleteBookmark(e) {
+    const btn = e.target.closest('#delete-btn');
+    const index = Number(btn.dataset.index);
+    console.log(index);
+
+    if (!btn) {
+        return;
+    }
+
     allBookmarks.splice(index, 1);
     localStorage.setItem('allBookmarks', JSON.stringify(allBookmarks))
     renderBookmark();
